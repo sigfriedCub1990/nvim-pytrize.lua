4a9e0abc5cf378a77b5c302e5f113d19a3013dbe
Ruben Garcia <rssanjuan2704@gmail.com>
2026-02-17 19:22:09 +0100

    ci: Run tests on PR opened

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,10 +1,15 @@
 local plenary_dir = "/tmp/plenary.nvim"
-local ts_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter"
+local ts_dirs = {
+  vim.fn.stdpath("data") .. "/lazy/nvim-treesitter",
+  "/tmp/nvim-treesitter",
+}
 
 vim.opt.runtimepath:append(".")
 vim.opt.runtimepath:append(plenary_dir)
-if vim.fn.isdirectory(ts_dir) == 1 then
-  vim.opt.runtimepath:append(ts_dir)
+for _, ts_dir in ipairs(ts_dirs) do
+  if vim.fn.isdirectory(ts_dir) == 1 then
+    vim.opt.runtimepath:append(ts_dir)
+  end
 end
 
 vim.cmd("runtime plugin/plenary.vim")
