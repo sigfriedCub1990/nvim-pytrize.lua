958d8afe56229a09c1c5c5ad1e85e9435c3a96ee
Ruben Garcia <rssanjuan2704@gmail.com>
2026-02-21 21:54:29 +0100

    fix: use vim.fn.execute() to suppress file info on jump
    Both ':silent edit' and temporarily setting shortmess+=F failed because
    Neovim's C-level fileinfo message can be displayed after the Lua frame
    completes, by which point shortmess has been restored and :silent no
    longer applies.
    
    vim.fn.execute() is backed by :redir, which captures all output at the
    channel level before it reaches the command line. This gives reliable
    suppression regardless of message timing. Also adds fnameescape() for
    correct handling of paths with special characters.

--- lua/pytrize/jump/util.lua
+++ lua/pytrize/jump/util.lua
@@ -1,10 +1,7 @@
 local M = {}
 
 M.open_file = function(file)
-    local saved = vim.o.shortmess
-    vim.opt.shortmess:append("F")
-    vim.cmd("edit " .. file)
-    vim.o.shortmess = saved
+    vim.fn.execute("edit " .. vim.fn.fnameescape(file))
 end
 
 return M
